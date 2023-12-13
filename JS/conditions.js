6e1ed9901d93e816b8ee55abc363c0e8921ab4a0
priti2790 <pritijadhav2790@gmail.com>
2023-12-13 15:58:29 +0530

    stringmethod

---
+++ JS/conditions.js
@@ -0,0 +1,38 @@
+// if else
+var marks = 34;
+     if(marks >=35 && marks <=50) {
+        console.log("Pass");
+    } else if (marks >=51 && marks <=60){
+        console.log("second class");
+    } else if (marks >=61 && marks <=70){
+        console.log("First class");
+    } else if (marks >=71 && marks <=100){
+        console.log("Distinction");
+    }else  {console.log("Fail");}
+
+    // switch
+let amount = 100;
+switch (amount) {
+    case 50:
+        console.log("You have 50Rs");
+        break;
+    case 60:
+        console.log("you have 60Rs");
+        break;
+    case 70:
+        console.log("You have 70Rs");
+        break;
+    case 80:
+        console.log("You have 80Rs");
+        break;
+    case 90:
+        console.log("You have 90Rs");
+        break;
+    case 100:
+        console.log("You have 100Rs");
+        break;
+    default:
+        console.log("You have no money");
+        break;
+}
+    
