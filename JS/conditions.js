bf81f049ef0e411de599aa97a70610cb80290734
priti2790 <pritijadhav2790@gmail.com>
2023-12-21 16:11:35 +0530

    operator

--- JS/conditions.js
+++ JS/conditions.js
@@ -35,4 +35,48 @@ switch (amount) {
         console.log("You have no money");
         break;
 }
-    
+    // comparison operaator
+    let a = 80; 
+    b = 90;
+    if(b == "90"){ //it will check only value not datatype
+        console.log("b has 90");
+    } 
+    if(b != 80){
+        console.log("b is not 80");
+    }
+    c = 100;
+    if(c === 100){ // it checks both   value and data type
+        console.log("Priti");
+    }
+    let d = true;
+    if(d){
+        console.log("D is true");
+    }
+    let e = 10; //condition will be true bcoz it has some value and compiler assumes it as true
+    if(e){
+        console.log("10");
+    }
+    let f = undefined; 
+    if(f){
+        console.log("undefined ");
+    }
+    let g = null;
+    if(g){
+        console.log("null");  //undefined//null will go false
+    }
+if(!g){
+    console.log("positive");
+}
+// &&
+let mark = 50;
+if(mark == 50 && mark >35 && mark >45){
+    console.log("Pass");
+} 
+// || logical OR
+let x = 50;
+let y = 20;
+if(x > 50 || y !=20 || y >10){
+    console.log("true");
+}
+// ternory operator
+marks > 35 ? console.log("pass") :console.log("Fail");
