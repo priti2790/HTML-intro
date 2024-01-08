0774e2aaf8611d9671e6c698ef71927045ee0135
priti2790 <pritijadhav2790@gmail.com>
2024-01-08 19:16:22 +0530

    data binding

--- angularproject/myapp/src/app/app.component.ts
+++ angularproject/myapp/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +9,15 @@ export class AppComponent {
   title = 'myapp';
       // property
 b : number =90;
+// property binding
+userName:string="Peter";
+// event binding
+handleclick(){
+  console.log("button is clicked");
+}
+// class binding
+mycssclass = "red";
+applycssclass = false;
+// two way data binding
+username:string="";
 }
