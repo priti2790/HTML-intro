9b7752e3cb7449874c81c9d6d3725b43e4f32df4
priti2790 <pritijadhav2790@gmail.com>
2024-02-17 22:21:50 +0530

    navbar

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-   <div>
+   <div class="navbar">
+    <a href="#home">Home</a>
+  <a href="#about">About</a>
+  <a href="#contact">Contact</a>
+  <a href="#services">Services</a>
+  <button id="cyan"class="light">light</button>
+  const button = document.getElementById('cyan');
+  button.addEventListener('click', function() {
     
+  }
+  <button id="black"class="dark">dark</button>
    </div>
   );
 }
